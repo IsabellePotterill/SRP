9a7c17c13d2b52cf66ac728b68cc157d08ca4c48
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-02 13:10:04 +0100

    Add files via upload

---
+++ accessions.py
@@ -0,0 +1,33 @@
+import os
+
+liston =[]
+with open("PRJNA281204.txt") as lines1:
+	lines = lines1.readlines()
+	for row in lines:
+		#print(row)
+		lol = row.split('\t')
+	#print(lol)
+		liston.append(lol)
+		with open('SraInfo.tsv', 'w') as ref:
+			#ent = ['Cell_no'] 
+			#ref.write('\t'.join(ent)+ '\t')
+			#dat = ['Data'] 
+			#ref.write('\t'.join(dat)+ '\t')
+			#lel = ['ID'] 
+			#ref.write('\t'.join(lel)+ '\n')
+			#print(lol)
+			
+			#print(liston)
+			info = [aa[3] for aa in liston]
+			print(info)
+			for wow in info:
+				uff = '\t'.join(wow)
+				print(uff)
+				ref.writelines(uff + '\n')
+        		
+
+	
+
+
+
+
