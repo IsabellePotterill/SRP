ce5753630f7e0026d8edde255918086b5409821b
GrahamHill <49273038+GrahamHill@users.noreply.github.com>
2019-07-06 17:05:14 +0100

    Update first.sh

--- first.sh
+++ first.sh
@@ -5,6 +5,8 @@ path="/home/graham/Downloads/trial/Data/"
 #loop through files in Dir
 for i in /home/graham/Downloads/trial/Data/*
 do
+	mkdir Junk
+	cd Junk
 	#suffixes for paired reads
 	one="_1.fastq.gz"
 	two="_2.fastq.gz"
@@ -19,8 +21,25 @@ do
 	#runs prinseq++ as specficied in paper on the 2 fastq files
 	#not sure about the setting on lc_entropy, but it seems to work
 	prinseq++ -min_len 30 -trim_left 10 -trim_qual_right 25 -lc_entropy 0.65 -threads 3 -fastq $one -fastq2 $two -out_name $j -out_bad /dev/null/ -out_bad2 /dev/null/ -out_single /dev/null/ -out_single2 /dev/null/
-	#the output from this doesn't work with '|' (pipe) yet, as it doesn't seem to appear in stdout
-	#we can just keep outputting, using and deleting each file as we go (otherwise we'll run out of storage)
-	#tar zcf $j.tar.gz 
-done
+	#set the suffixes to the new output pair
+	one="_good_out_R1.fastq"
+	two="_good_out_R2.fastq"
+	fastqc $j$one $j$two --extract -t 3
+	parse_fastqc1=$(python3 fafapa_call.py $j"_good_out_R1")
+	parse_fastqc2=$(python3 fafapa_call.py $j"_good_out_R2")
+	echo "output of python "$parse_fastqc1
+	echo "output of python "$parse_fastqc2
+	cutadapt $parse_fastqc1 -e 0.15 -m 30 -o cutadR1$j.fastq -j 3 $j$one
+	cutadapt $parse_fastqc2 -e 0.15 -m 30 -o cutadR2$j.fastq -j 3 $j$one
+	prinseq++ -min_len 30 -fastq cutadR1$j.fastq -fastq2 cutadR2$j.fastq -out_name trimmed$j -out_bad /dev/null/ -out_bad2 /dev/null/ -out_single /dev/null/ -out_single2 /dev/null/
+	#CAN TRIM_GALORE TAKE PAIRED FILES??? YES! How does this work?
+	trim_galore trimmed$j$one trimmed$j$two --stringency 1 --nextera --paired
+	mv "trimmed"$j"_good_out_R1_trimmed.fq" "/home/graham/Downloads/trial/Data/Results"
+	mv "trimmed"$j"_good_out_R1_trimmed.fq" "/home/graham/Downloads/trial/Data/Results"
+	cd ..
+	#IS THIS SAFE?!
+	rm -rf Junk
 
+	#STAR $whatever -outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outSAMstrandField intronMotif
+	#HTSeq -m intersection-nonempty -s no
+done
