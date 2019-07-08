575670e1062c69a435cb968ad8ad75cc3457887d
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-08 13:01:45 +0100

    Add files via upload

---
+++ pipeline_cell_type.py
@@ -0,0 +1,23 @@
+liston =[]
+with open("SraRunTable.txt") as file:
+    lines = file.readlines()
+    for row in lines:
+        lol = row.split('\t')
+        liston.append(lol)
+        #print(liston)
+        with open('furtherInfoPipe.tsv', 'w') as ref:
+       		info = [[aa[9],aa[11],aa[10],aa[12]] for aa in liston]
+       		#print(info)
+       		for item in info:
+       			wow = '\t'.join(item)
+       			ref.writelines(wow+'\n')
+
+        with open('pipe2.tsv', 'w') as ref:
+       		info = [[aa[9],aa[12]] for aa in liston]
+       		#print(info)
+       		for item in info:
+       			wow = '\t'.join(item)
+       			ref.writelines(wow+'\n')
+
+
+
