980f90dbb4b94030aa51e8baaa6d9c749b7e77cf
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-11 09:35:17 +0100

    Update BAM_alignment_RSUBREAD_+FurtherIndications.R

--- BAM_alignment_RSUBREAD_+FurtherIndications.R
+++ BAM_alignment_RSUBREAD_+FurtherIndications.R
@@ -4,13 +4,12 @@ library(SummarizedExperiment)
 library(Rsubread)
 
 
-#buildindex(basename, reference, gappedIndex = FALSE, 
-#           indexSplit = FALSE,memory = 8000, TH_subread = 100, colorspace = FALSE)
-
 #we need alignment file: human-genome-reference
-ERCCfa_fn = file.path("/home/rpg18/Downloads/Homo_sapiens.GRCh38.cdna.all.fa.gz") #I have tried with cDNA
-#we need annotation file: human-annotation-reference
-ERCCanno_fn = file.path("/home/rpg18/Downloads/gencode.v28.annotation.gff3.gz")
+ERCCfa_fn = file.path("/home/rpg18/Downloads/GRCh38.primary_assembly.genome.fa.gz")
+
+#Builds the index of human-genome-reference
+Rsubread::buildindex(basename=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/Index38_GENCODE/", "ERCC_index"), 
+                     reference=ERCCfa_fn, gappedIndex = TRUE)
 
 #Read1 for cell1
 fq_R1 = file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/SRR1974543_1.fastq.gz")
@@ -25,9 +24,6 @@ sc_trim_barcode(file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/
                 fq_R2,
                 read_structure = list(bs1=0, bl1=0, bs2=0, bl2=0, us=0, ul=0)) #we do not have barcodes, we do not need them
 
-#Builds the index of human-genome-reference
-Rsubread::buildindex(basename=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/Index_ensembl_cdna38/", "ERCC_index"), 
-                     reference=ERCCfa_fn, gappedIndex = TRUE)
 
 #Now we can proceed with the alignment. In this step we could use just one readfile (both reads together-combined.fastq file\\ 
 #or we could have used readfile1 for the first read, and readfile2 for the second read [not sure if this is possible with both\\
@@ -36,20 +32,15 @@ Rsubread::align(index=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_
                 readfile1=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "combined.fastq.gz"),
                 #readfile1=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/", "SRR1974543_1.fastq.gz"),
                 #readfile2=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/", "SRR1974543_2.fastq.gz"),
-                output_file=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "out2.aln.bam"), type=0) #type=0 for RNA-seq
+                output_file=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "out_final.aln.bam"), type=0) #type=0 for RNA-seq
 
-#With this output: out2.aln.bam, we are ready for the counting
+#With this output: out_final.aln.bam, we are ready for the counting
 
-#We can sort by names the .bam file using the command-line: 
+#The BAM file is already sorted. If not, we could sort by names the .bam file using the command-line: 
 #samtools sort -n out.aln.bam -o out_sort.bammtools sort -n 
 
 #We now run HTSeq (htseq-count) to generate the counting matrix:
 #Quick installation of HTSeq:
 #pip install HTSeq
-#htseq-count 
-####################################
-
-#Example of annotation following scPipe tutorial, this is optional. We actually are not trying this, but we could
-sc_exon_mapping(file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "out.aln.bam"),
-                 file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "out.map.bam"),
-                 ERCCanno_fn)
+##HTSeq:
+#htseq-count -r name -f bam -s no --additional gene_name out_final.aln.bam /home/rpg18/Downloads/gencode.v28.annotation.gff3.gz > output_CountsMatrix.csv
