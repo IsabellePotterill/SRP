2d5129847d78a66fd17b1ef335f3479b2c325ab7
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-11 11:48:38 +0100

    Update BAM_alignment_RSUBREAD_+FurtherIndications.R

--- BAM_alignment_RSUBREAD_+FurtherIndications.R
+++ BAM_alignment_RSUBREAD_+FurtherIndications.R
@@ -10,6 +10,7 @@ library(Rsubread)
 #BiocManager::install("Rsubread")
 
 #we need alignment file: human-genome-reference
+#ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/GRCh38.primary_assembly.genome.fa.gz
 ERCCfa_fn = file.path("/home/rpg18/Downloads/GRCh38.primary_assembly.genome.fa.gz")
 
 #Builds the index of human-genome-reference
