19c09725dc3f6e9e58aebb28ae4b8d74dc7af5cf
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-21 19:51:25 +0100

    Update sc3_1_pipeline_cell_type.py

--- sc3_1_pipeline_cell_type.py
+++ sc3_1_pipeline_cell_type.py
@@ -1,25 +1,18 @@
 #pipe2.tsv is the file that we need to generate a "sce" object, which contains tow columns: sample_name and cell_type.
 
-liston =[]
+list =[]
 with open("SraRunTable.txt") as file:
     lines = file.readlines()
     for row in lines:
-        lol = row.split('\t')
-        liston.append(lol)
-        #print(liston)
-        #with open('furtherInfoPipe.tsv', 'w') as ref:
-       	#	info = [[aa[9],aa[11],aa[10],aa[12]] for aa in liston]
-       		#print(info)
-       	#	for item in info:
-       	#		wow = '\t'.join(item)
-       	#		ref.writelines(wow+'\n')
+        line = row.split('\t')
+        list.append(lol)
 
-        with open('pipe2.tsv', 'w') as ref:
-       		info = [[aa[9],aa[12]] for aa in liston]
+        with open('pipeline_info.tsv', 'w') as ref:
+       		info = [[aa[9],aa[12]] for aa in list]
        		#print(info)
        		for item in info:
-       			wow = '\t'.join(item)
-       			ref.writelines(wow+'\n')
+       			feature = '\t'.join(item)
+       			ref.writelines(feature+'\n')
 
 
 
