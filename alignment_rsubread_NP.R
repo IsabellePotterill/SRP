7783eae72db7bc9edbd62c17cfdf4bb49bdf26e0
rpg18 <49445224+rpg18@users.noreply.github.com>
2019-07-23 18:17:44 +0100

    Update alignment_rsubread_NP.R

--- alignment_rsubread_NP.R
+++ alignment_rsubread_NP.R
@@ -1,48 +1,33 @@
-library(scPipe)
 library(SingleCellExperiment)
-library(SummarizedExperiment)
 library(Rsubread)
 
-#BiocManager::install("SC3")
 #BiocManager::install("SingleCellExperiment")
-#BiocManager::install("SummarizedExperiment")
 #BiocManager::install("RUVSeq")
 #BiocManager::install("Rsubread")
-#BiocManager::install("scPipe")
 
-#we need alignment file: human-genome-reference
+## Inputs ----
+# alignment file: human-genome-reference
 #ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/GRCh38.primary_assembly.genome.fa.gz
 ERCCfa_fn = file.path("/home/rpg18/Downloads/GRCh38.primary_assembly.genome.fa.gz")
 
 #Builds the index of human-genome-reference
-Rsubread::buildindex(basename=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/Index38_GENCODE/", "ERCC_index"), 
+Rsubread::buildindex(basename=file.path("/path/Index38_GENCODE/", "ERCC_index"), 
                      reference=ERCCfa_fn, gappedIndex = TRUE)
+# Read1
+fq_R1 = file.path("/path/_1.fastq.gz")
+# Read2
+fq_R2 = file.path("/path/_2.fastq.gz")
 
-#Read1 for cell1
-fq_R1 = file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/SRR1974543_1.fastq.gz")
-#Read2 for cell2
-fq_R2 = file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/SRR1974543_2.fastq.gz")
+## Alignment ----
+# Align each read with the human genome reference:
+Rsubread::align(index=file.path("/path/Index38_GENCODE/", "ERCC_index"),
+                readfile1=fq_R1,
+                readfile2=ffq_R2,
+                output_file=file.path("/path/", "out_final.aln.bam"), type=0) #type=0 for RNA-seq
 
+#With this output: out_final.aln.bam, we are ready for the counting matrix creation
 
-#This step is just needed to create a combined.fastq file.(Both reads in same .bam file after alignment)
-#we could do this in another way, maybe with fastp
-#sc_trim_barcode(file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "combined.fastq.gz"),
-#                fq_R1,
-#                fq_R2,
-#                read_structure = list(bs1=0, bl1=0, bs2=0, bl2=0, us=0, ul=0)) #we do not have barcodes, we do not need them
-
-
-#Now we can proceed with the alignment. In this step we could use just one readfile (both reads together-combined.fastq file\\ 
-#or we could have used readfile1 for the first read, and readfile2 for the second read [not sure if this is possible with both\\
-#at the same time or individually])
-Rsubread::align(index=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/Index38_GENCODE/", "ERCC_index"),
-                #readfile1=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "combined.fastq.gz"),
-                readfile1=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/", "SRR1974543_1.fastq.gz"),
-                readfile2=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/reads/", "SRR1974543_2.fastq.gz"),
-                output_file=file.path("/home/rpg18/Desktop/SRP_article/ALL_DATA/Fastq_files/", "out_final.aln.bam"), type=0) #type=0 for RNA-seq
-
-#With this output: out_final.aln.bam, we are ready for the counting
-
+## Not_included: ----
 #The BAM file is already sorted. But we could have sorted the BAM file by names (for example) using SAMTOOLS (command-line): 
 #samtools sort -n out.aln.bam -o out_sort.bammtools sort -n 
 
